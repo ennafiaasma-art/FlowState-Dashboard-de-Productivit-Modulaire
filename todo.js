945de05193dc827ba68fc14c4dec695c5cc2fdc3
ennafiaasma-art <ennafiaasma@gmail.com>
2026-02-10 17:45:16 +0100

    creer la fonction de l'ajoute en html et javaScript

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const btn=document.getElementById("btn");
 const form=document.querySelector("form")
 const ul=document.createElement("ul");
+const deletbtn=document.getElementById("deletbtn")
 
 const creat =document.getElementById("creat")
 document.body.appendChild(ul);
@@ -10,4 +11,25 @@ form.addEventListener("submit",(e)=>{
     const li=document.createElement("li");
      li.textContent=creat.value
     ul.appendChild(li)
+    ul.innerHTML=`<form>  <form >
+
+
+        <div class="py-5 px-5 d-flex justify-between" >
+            <img src="" alt="">
+            <h1></h1>
+            <p>${}</p>
+            
+
+        </div>
+    </form>
+    
+    
+    
+    
+    
+    </form>`
+    
+
 })
+
+
